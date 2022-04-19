22c3578da3acb3138d6c6258dbba34774b9c56ae
Latkin Mikhail <82706373+lmfl1984@users.noreply.github.com>
2022-04-19 07:19:56 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,4 +1,15 @@
 
 "use strict" //new function ON
 
-console.log('Fist text on JS');
+/* *******************
+   Нажатие на бургер
+******************* */
+const iconMenu = document.querySelector('.menu__icon');
+if (iconMenu) {
+   const menuBody = document.querySelector('.header__menu');
+   iconMenu.addEventListener("click", function(e) {
+      document.body.classList.toggle('_lock');
+      iconMenu.classList.toggle('_active');
+      menuBody.classList.toggle('_active');
+   });
+};
